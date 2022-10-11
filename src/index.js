2d14d653cbcae4746de10dfab75a5c6955f071fc
Bayram Enes YILMAZ <63585653+bayramenesyilmaz@users.noreply.github.com>
2022-10-11 18:39:50 +0300

    update route

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ import App from './App';
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
